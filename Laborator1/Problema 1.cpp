8fea0f037ea0367ac8227c247127bda75ef98dcb
Paul513607 <79131709+Paul513607@users.noreply.github.com>
2021-02-18 09:16:39 +0200

    Update Problema 1.cpp

--- Laborator1/Problema 1.cpp
+++ Laborator1/Problema 1.cpp
@@ -5,11 +5,28 @@ using namespace std;
 
 ifstream fin ("ini.txt");
 
+int convert_to_int(char x[])
+{
+    int i,nr=0,ok=1;
+    for (i=0; x[i]; ++i)
+    {
+        if (x[i]>='0' && x[i]<='9')
+            nr=nr*10+(x[i]-'0')%10;
+        else ok=0;
+    }
+    if (ok==0) return 0;
+    return nr;
+}
+
 int main()
 {
-    int x,s=0;
+    char x[20];
+    int s=0,nr;
     while (fin >> x)
-        s+=x;
+    {
+        nr=convert_to_int(x);
+        s+=nr;
+    }
     cout << s;
     return 0;
 }
