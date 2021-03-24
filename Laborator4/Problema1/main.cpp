55dfa424bdc663b8f07582398e078e4b9d71625a
Paul513607 <79131709+Paul513607@users.noreply.github.com>
2021-03-24 21:36:45 +0200

    Update main.cpp

--- Laborator4/Problema1/main.cpp
+++ Laborator4/Problema1/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    bool ascendent=false;
+    bool ascendent=true;
     int v[100]={1,2,3,4,10,2};
     Sort d1(10,50,150),d2({1,2,3,4,2,5}),d3(6,v),d4(5,9,0,14,20,18),d5("10,40,100,5,70");
     d1.Print();
@@ -13,11 +13,11 @@ int main()
     d4.Print();
     d5.Print();
     std::cout << '\n';
-    d1.InsertSort(ascendent);
+    d1.InsertSort(true);
     d1.Print();
-    d3.BubbleSort(ascendent);
+    d3.BubbleSort(false);
     d3.Print();
-    d5.QuickSort(ascendent);
+    d5.QuickSort(true);
     d5.Print();
     std::cout << '\n';
     std::cout << d1.GetElementsCount() << " " << d4.GetElementFromIndex(3);
